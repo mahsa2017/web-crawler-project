5b607147310361954e4ede9a3d8fda54febc5430
bltc-17 <chrismnewton@icloud.com>
2018-11-06 13:05:04 +0000

    file downloads when link clicked

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	state = {
+		download: false
+	};
 	render() {
 		return (
 			<div className="App">
 				<form onSubmit={this.handleSubmit}>
-					<input placeholder="Please enter URL here" />
+					<p>Please select one of the following links</p>
+					<select>
+						<option value="https://broken-links-api.herokuapp.com/">https://broken-links-api.herokuapp.com/</option>
+						<option value="https://web-crawler-test1.herokuapp.com/">https://web-crawler-test1.herokuapp.com/</option>
+						<option value="https://web-crawler-test2.herokuapp.com/">https://web-crawler-test2.herokuapp.com/</option>
+						<option value="https://random-static-linky-site-01.herokuapp.com/">https://random-static-linky-site-01.herokuapp.com/</option>
+						<option value="https://random-static-linky-site-02.herokuapp.com/">https://random-static-linky-site-02.herokuapp.com/</option>
+					</select>
 					<button>Submit URL</button>
 				</form>
+				{this.state.download && (
+					<a href="example.txt" download>
+						Start Automatic Download
+					</a>
+				)}
 			</div>
 		);
 	}
 	handleSubmit = event => {
 		event.preventDefault();
+		this.setState({
+			download: true
+		});
 	};
 }
 
